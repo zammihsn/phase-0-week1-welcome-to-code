561668b2bde38a9d645c3f0f2bb693cecd81b3d2
zammihsn <azzamihsan@gmail.com>
2026-07-11 20:25:39 +0700

    submit tugas phase0 week1 part2

---
+++ quiz/part2/soal3.js
@@ -0,0 +1,19 @@
+// Problem
+// Mirip seperti soal nomor 2, namun kali ini gunakan substring untuk mengambil potongan dari tiap kata!
+
+let word3 = 'wow JavaScript is so cool';
+let exampleFirstWord3 = word3.substring(0, 3);
+
+console.log('First Word: ' + exampleFirstWord3);
+
+let secondWord3 = word3.substring(4, 14);
+console.log('Second Word: ' + secondWord3);
+
+let thirdWord3 = word3.substring(15, 17);
+console.log('Third Word: ' + thirdWord3);
+
+let fourthWord3 = word3.substring(18, 20);
+console.log('Fourth Word: ' + fourthWord3);
+
+let fifthWord3 = word3.substring(21, 25);
+console.log('Fifth Word: ' + fifthWord3);
---
+++ quiz/part2/soal4.js
@@ -0,0 +1,24 @@
+// Problem
+// Mirip seperti soal nomor 3, tapi tampilkan juga panjang kata masing-masingnya!
+
+
+let word4 = 'wow JavaScript is so cool';
+let exampleFirstWord4 = word4.substring(0, 3);
+let firstWordLength = exampleFirstWord4.length;
+console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + firstWordLength);
+
+let secondWord4 = word4.substring(4, 14);
+let secondWordLength = secondWord4.length;
+console.log('Second Word: ' + secondWord4 + ', with length: ' + secondWordLength);
+
+let thirdWord4 = word4.substring(15, 17);
+let thirdWordLength = thirdWord4.length;
+console.log('Third Word: ' + thirdWord4 + ', with length: ' + thirdWordLength);
+
+let fourthWord4 = word4.substring(18, 20);
+let fourthWordLength = fourthWord4.length;
+console.log('Fourth Word: ' + fourthWord4 + ', with length: ' + fourthWordLength);
+
+let fifthWord4 = word4.substring(21, 25);
+let fifthWordLength = fifthWord4.length;
+console.log('Fifth Word: ' + fifthWord4 + ', with length: ' + fifthWordLength);
